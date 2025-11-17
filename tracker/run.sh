a98d4fc3a6e749910b0239db6df992cfe2fc157c
seanalewine <3515854+seanalewine@users.noreply.github.com>
2025-11-17 14:38:19 -0600

    fix flags

--- tracker/run.sh
+++ tracker/run.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/with-contenv bashio
 website_name=$(bashio::config 'website_name')
 
-touch /data/test_write.txt
-if [ $? -eq 0 ]; then
-    rm /data/test_write.txt
-    echo "SUCCESS: Write access verified."
-else
-    echo "ERROR: Permission denied to write to /data."
-fi
-
-cp -v /files/. /data/
+cp -rnv /files/. /data/
 
 echo $(ls /data)
 
