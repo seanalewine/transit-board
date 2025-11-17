8d55f448d4fa00a567d41493114de7764810ee6f
seanalewine <3515854+seanalewine@users.noreply.github.com>
2025-11-17 14:21:01 -0600

    test file copying

--- tracker/run.sh
+++ tracker/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 website_name=$(bashio::config 'website_name')
 
-echo "Hello world!"
-
 echo "<h1>Not for humans</h1>" > /data/index.html
+
+echo $(ls /files)
 
 python3 -m http.server 8000
